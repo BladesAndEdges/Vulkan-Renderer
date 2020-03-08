094f836ba875e27ab2104ee8b11f3652dbffe68d
Daniel Banski [sc16dbb] <sc16dbb@leeds.ac.uk>
2020-03-08 18:54:12 +0000

    Deleted useless files

--- QuackQuack/TriangleFaceNormal.cpp
+++
@@ -1,5 +0,0 @@
-#include "TriangleFaceNormal.h"
-
-TriangleFaceNormal::TriangleFaceNormal(uint32_t main, uint32_t first, uint32_t second) : mainNormalIndex(main), firstNormalIndex(first), secondNormalIndex(second)
-{
-}
--- QuackQuack/TriangleFacePosition.cpp
+++
@@ -1,5 +0,0 @@
-#include "TriangleFacePosition.h"
-
-TriangleFacePosition::TriangleFacePosition(uint32_t main, uint32_t first, uint32_t second) : mainVertexIndex(main), firstVertexIndex(first), secondVertexIndex(second)
-{
-}
