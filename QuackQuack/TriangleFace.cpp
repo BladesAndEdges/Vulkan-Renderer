370681ade7cd3dd46f1c62df0b3365c0cf231cf6
Daniel Banski [sc16dbb] <sc16dbb@leeds.ac.uk>
2020-03-08 19:00:44 +0000

    Deleted these files

--- QuackQuack/TriangleFace.cpp
+++
@@ -1,5 +0,0 @@
-#include "TriangleFace.h"
-
-TriangleFace::TriangleFace(uint32_t main, uint32_t first, uint32_t second) : mainVertexIndex(main), firstVertexIndex(first), secondVertexIndex(second)
-{
-}
